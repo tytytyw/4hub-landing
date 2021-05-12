e2bca16da87ec29e6978e52809d9e1f1494eed87
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-05-12 22:22:55 +0300

    changed colors collection

--- src/generalComponents/collections.js
+++ src/generalComponents/collections.js
@@ -2,16 +2,16 @@ export const tags = ['Фото', 'Видео', 'Музыка', 'Документ
 
 export const colors = [
     {dark: '#efeeee', light: '#efeeee', color: 'grey'},
-    {dark: '#c4aa8f', light: '#FEDCB8', color: 'wheat'},
-    {dark: '#326bc1', light: '#4086F1', color: 'blue'},
-    {dark: '#27967a', light: '#34C6A2', color: 'green'},
-    {dark: '#cb8d40', light: '#F9AD50', color: 'orange'},
-    {dark: '#c15862', light: '#F4707D', color: 'pink'},
-    {dark: '#792794', light: '#A234C6', color: 'purple'},
-    {dark: '#992928', light: '#C63534', color: 'red'},
-    {dark: '#267d8d', light: '#34AFC6', color: 'aqua'},
-    {dark: '#a0602a', light: '#C67734', color: 'brown'},
-    {dark: '#40289b', light: '#5134C6', color: 'darkblue'}
+    {dark: '#c4aa8f', light: 'rgb(254, 220, 184)', color: 'rgb(254, 220, 184)', name: 'wheat'},
+    {dark: '#326bc1', light: 'rgb(64, 134, 242)', color: 'rgb(64, 134, 242)', name: 'blue'},
+    {dark: '#27967a', light: 'rgb(52, 198, 162)', color: 'rgb(52, 198, 162)', name: 'green'},
+    {dark: '#cb8d40', light: 'rgb(249, 173, 80)', color: 'rgb(249, 173, 80)', name: 'orange'},
+    {dark: '#c15862', light: 'rgb(244, 112, 125)', color: 'rgb(244, 112, 125)', name: 'pink'},
+    {dark: '#792794', light: 'rgb(160, 51, 195)', color: 'rgb(160, 51, 195)', name: 'purple'},
+    {dark: '#992928', light: 'rgb(194, 52, 51)', color: 'rgb(194, 52, 51)', name: 'red'},
+    {dark: '#267d8d', light: 'rgb(52, 174, 196)', color: 'rgb(52, 174, 196)', name: 'aqua'},
+    {dark: '#a0602a', light: 'rgb(196, 118, 151)', color: 'rgb(196, 118, 151)', name: 'brown'},
+    {dark: '#40289b', light: 'rgb(81, 52, 197)', color: 'rgb(81, 52, 197)', name: 'darkblue'}
 ];
 
 export const signs = ['triangle', 'circle', 'heart', 'star', 'pentagon', 'rectangle', 'notification', 'stop', 'shield-2', 'like', 'price-tag'];
