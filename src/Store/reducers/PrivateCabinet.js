37efc5fd949bfef1193155c42454378baa85914f
Ravshan Davlatov <ravshandavlatov014@gmail.com>
2021-05-17 09:36:34 +0500

    Add, Edit,  Delete, Send Contacts

--- src/Store/reducers/PrivateCabinet.js
+++ src/Store/reducers/PrivateCabinet.js
@@ -3,14 +3,9 @@ import {
     CHOOSE_FOLDER,
     CHOOSE_FILES,
     FILE_DELETE,
-<<<<<<< HEAD
-    CONTACT_LIST
-=======
     CONTACT_LIST,
-    ADD_CONTACT,
     ADD_RECENT_FOLDERS,
-    ADD_RECENT_FILES,
->>>>>>> fe1f798acfaeafe28a2664e68fb7fd8a2b9c512a
+    ADD_RECENT_FILES
 } from '../types';
 
 const INITIAL_STATE = {
@@ -40,15 +35,10 @@ export default function startPage(state = INITIAL_STATE, action) {
         }
         case CONTACT_LIST:
             return {...state, contactList: action.payload}
-<<<<<<< HEAD
-=======
-        case ADD_CONTACT:
-            return {...state, contactList: [...state.contactList, action.payload]}
         case ADD_RECENT_FOLDERS:
             return {...state, recentFolders: action.payload}
         case ADD_RECENT_FILES:
             return {...state, recentFiles: action.payload}
->>>>>>> fe1f798acfaeafe28a2664e68fb7fd8a2b9c512a
         default:
             return state;
     }
