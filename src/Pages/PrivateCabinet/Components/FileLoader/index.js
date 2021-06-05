e41f3dc13f1b54d74896fe791ac0e0ff6be7b22c
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-06-05 13:32:57 +0300

    added repeat button to FileLoader

--- src/Pages/PrivateCabinet/Components/FileLoader/index.js
+++ src/Pages/PrivateCabinet/Components/FileLoader/index.js
@@ -64,20 +64,22 @@ const FileLoader = ({
             data.append('uid', uid);
             data.append('myfile', file.file);
             data.append('fileName', `${file?.options?.name ? file.options.name.slice(0, file.options.name.lastIndexOf('.')) : file.file.name.slice(0, file.file.name.lastIndexOf('.'))}`);
-            data.append('dir', path ? path : 'global/all');
+            data.append('dir', file.filePath ? file.filePath : 'global/all');
             data.append('tag', file?.options?.tag ? file.options.tag : '');
             data.append('pass', file?.options?.password ? file.options.password : '');
             data.append('color', file?.options?.color ? file.options.color : '');
             data.append('symbol', file?.options?.symbol ? file.options.symbol : '');
             data.append('emoji', file?.options?.emoji ? file.options.emoji : '');
-            await api.post(`/ajax/file_add.php`,
-                data,
-                {
-                    onUploadProgress: e => {setProcessing((e.loaded * 100) / e.total)},
-                    cancelToken: new CancelToken(function executor(e){const cancelLoading = e; setOptions({cancelLoading})})
-                })
-                .then(res => setResponse({res, file}))
-                .catch(err => console.log(err));
+
+            console.log(data)
+            // await api.post(`/ajax/file_add.php`,
+            //     data,
+            //     {
+            //         onUploadProgress: e => {setProcessing((e.loaded * 100) / e.total)},
+            //         cancelToken: new CancelToken(function executor(e){const cancelLoading = e; setOptions({cancelLoading})})
+            //     })
+            //     .then(res => setResponse({res, file}))
+            //     .catch(err => console.log(err));
         }
     };
 
--- src/Pages/PrivateCabinet/index.js
+++ src/Pages/PrivateCabinet/index.js
@@ -14,6 +14,7 @@ import Programs from "./Components/Programs";
 const PrivateCabinet = () => {
 
     const uid = useSelector(state => state.user.uid);
+    const path = useSelector(state => state.PrivateCabinet.fileList?.path);
     const dispatch = useDispatch();
     const [menuItem, setItem] = useState('Мои папки');
     const [collapsed, setCollapsed] = useState(false)
@@ -40,7 +41,7 @@ const PrivateCabinet = () => {
     const [loadingFile, setLoadingFile] = useState([]);
     const [loaded, setLoaded] = useState([]);
     const onInputFiles = (e) => {
-        const files = [...e.target.files].map(file => {return {file, options: {}}});
+        const files = [...e.target.files].map(file => {return {file, options: {filePath: path}}});
         setAwaitingFiles([...awaitingFiles].concat(...files));
         inputRef.current.value = '';
     };
