1193dbff9e691dd808ba4796cbf2b15924a6481c
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-06-29 09:28:35 +0300

    added formats file to open for PrivateCabinet

--- src/generalComponents/collections.js
+++ src/generalComponents/collections.js
@@ -99,4 +99,4 @@ export const contextMenuSubFolder = {
 }
 
 export const previewTypes = ['application/msword', 'application/excel'];
-export const previewFormats = ['doc', 'xls', 'ppt', 'txt'];
+export const previewFormats = ['doc', 'xls', 'ppt', 'rtf', 'xlt', 'csv'];
