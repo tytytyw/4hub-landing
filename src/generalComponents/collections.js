4d7333e5262606529e2512c3a3b3fb76374bf745
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-07-17 07:36:40 +0300

    deleted sgarefolder from context menu general folders for PrivateCabinet

--- src/generalComponents/collections.js
+++ src/generalComponents/collections.js
@@ -66,7 +66,7 @@ export const contextMenuProjects = {
 
 export const contextMenuFolderGeneral = {
     main: [
-        {name: 'Расшарить', img: 'resend'},
+        // {name: 'Расшарить', img: 'resend'},
         {name: 'Настроить доступ', img: 'shareFile'},
         {name: 'Скопировать сслылку', img: 'link-4'},
         {name: 'Добавить папку', img: 'settings-work-tool'},
