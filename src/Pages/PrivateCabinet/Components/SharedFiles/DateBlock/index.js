9d16a7ad5264fad2c2294be6299312ac1dfe8b83
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2021-07-23 13:14:51 +0300

    remove search field from DateBlock for SharedFiles

--- src/Pages/PrivateCabinet/Components/SharedFiles/DateBlock/index.js
+++ src/Pages/PrivateCabinet/Components/SharedFiles/DateBlock/index.js
@@ -4,7 +4,7 @@ import Select from '../../../../../generalComponents/Select/Select'
 import {getDays, getYears, months} from '../helper'
 import classNames from 'classnames'
 
-const DateBlock = ({search, setSearch, month, setMonth}) => {
+const DateBlock = ({month, setMonth}) => {
 
     return (
         <div className={styles.wrapper}>
@@ -17,21 +17,6 @@ const DateBlock = ({search, setSearch, month, setMonth}) => {
                         className={styles.select}
                         classNameSelect={styles.selectContentYear}
                         data={getYears()}
-                    />
-                </div>
-
-                <div className={styles.search}>
-                    <input
-                        type='search'
-                        value={search || ''}
-                        onChange={event => setSearch(event.target.value)}
-                        className={styles.input}
-                        placeholder='Введите ключевое слово'
-                    />
-                    <img
-                        className={styles.icon}
-                        src='./assets/PrivateCabinet/magnifying-glass-2.svg'
-                        alt='Search'
                     />
                 </div>
 
