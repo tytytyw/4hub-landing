a0eb349d37d8f79b60ca0385fc7cb7a529ec1f3f
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2021-08-20 20:43:05 +0300

    remove CopyLink from Safe

--- src/generalComponents/collections.js
+++ src/generalComponents/collections.js
@@ -131,7 +131,6 @@ export const contextMenuSubFolder = {
 export const contextMenuSafeItem = {
     main: [
         {name: 'Предоставить доступ', img: 'download-blue'},
-        {name: 'Скопировать ссылку', img: 'link-4'},
         {name: 'Редактировать', img: 'edit'},
         {name: 'Настроить', img: 'settings-work-tool'},
         {name: 'Свойства', img: 'info'},
