5cad41c48b0685641f108cef25c7ba87300ee20a
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-09-11 10:57:33 +0300

    added tests InputField => generalComponents

--- src/generalComponents/InputField/InputField.test.js
+++ src/generalComponents/InputField/InputField.test.js
@@ -3,16 +3,24 @@ import InputField from './index';
 import {mount} from 'enzyme';
 
 describe('InputField check tests', () => {
-    it('renders input', () => {
-        const props = {
-            value: '',
-            // set: (e) => {props.value = e.target.value},
-            // placeholder: 'renderer',
-        };
-        const wrapper = mount(<InputField {...props} />);
-        console.log(wrapper.find('inputField'));
 
-        // expect(wrapper.find('.inputField')).to.equal('');
-        // expect(wrapper.props('placeholder')).toBe('renderer');
+    const props = {
+        value: '',
+        set: jest.fn(),
+        placeholder: 'renderer',
+    }
+    const wrapper = mount(<InputField {...props} />);
+
+    it('renders input with initial values', () => {
+        expect(wrapper.find('input').prop('value')).toEqual('');
+        expect(wrapper.find('input').prop('placeholder')).toBe('renderer');
     });
+
+    it('should delete readOnly Property after focus', () => {
+        expect(wrapper.find('input').prop('readOnly')).toBe(true);
+        // console.log(wrapper.find('input').debug());
+        wrapper.find('input').simulate('focus');
+        console.log(wrapper.find('input').debug());
+        // expect(wrapper.find('input').prop('readOnly')).toBe(undefined);
+    })
 });
