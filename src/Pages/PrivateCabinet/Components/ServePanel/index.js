5dc01794fb74685042058a519e066dd2c0c0a3e2
Dmitriy_Kolesnikov <nonameFOX@yandex.ru>
2021-09-13 18:48:08 +0300

    fixed a typo in setFilter for ServePanel

--- src/Pages/PrivateCabinet/Components/ServePanel/index.js
+++ src/Pages/PrivateCabinet/Components/ServePanel/index.js
@@ -63,8 +63,8 @@ const ServePanel = ({
 
     const setFilter = (sorting) => {
         dispatch(onSortFile(sorting));
-        if (menuItem === 'MyFolders') dispatch(onChooseFiles(fileList.path, search, 1, '', ''))
-        if (menuItem === 'MyFiles') dispatch(onChooseAllFiles(fileList.path, search, 1, '', ''))
+        if (menuItem === 'myFolders') dispatch(onChooseFiles(fileList.path, search, 1, '', ''))
+        if (menuItem === 'myFiles') dispatch(onChooseAllFiles(fileList.path, search, 1, '', ''))
     };
 
     const createFile = (ext) => {
