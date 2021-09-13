7f68a622997568cb8aca6f74935417e79f3d2fc6
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-09-13 23:23:29 +0300

    clear warnings

--- src/Pages/PrivateCabinet/Components/PreviewFile/index.js
+++ src/Pages/PrivateCabinet/Components/PreviewFile/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React from 'react';
 
 import {previewFormats} from '../../../../generalComponents/collections';
 import styles from './PreviewFile.module.sass';
