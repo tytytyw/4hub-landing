ee83b4b5782fd077323eb536d67ee41da9e47688
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-11-28 09:26:58 +0200

    added FileInfo component to generalComponents

---
+++ src/generalComponents/FileInfo/FileInfo.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import styles from './FileInfo.module.sass'
+import classNames from "classnames";
+import File from "../Files";
+import {imageSrc} from "../globalVariables";
+
+function FileInfo({file = {}}) {
+    return(
+        <>
+            {file === {} ? null : <div className={styles.wrap}>
+            <div className={styles.innerFileWrap}>
+                <File color={file.id_color} format={file.ext} />
+                {file.is_pass ? <img className={styles.lock} src={`${imageSrc}assets/PrivateCabinet/locked.svg`} alt='lock' /> : null}
+            </div>
+            <div className={styles.descriptionWrap}>
+                <div className={styles.fileName}>{file.name}</div>
+                <div className={styles.innerFileInfo}>
+                    <div className={styles.fileSize}>{file?.size_now}</div>
+                    <div className={styles.descriptionGroup}>
+                        <div
+                            className={classNames({
+                                [styles.tagBlock]: true,
+                                [styles.ftag]: !!file?.tag,
+                            })}
+                        >
+                            {file?.tag && `#${file.tag}`}
+                        </div>
+                        {file.fig && (
+                            <img
+                                src={`${imageSrc}assets/PrivateCabinet/signs/${file.fig}.svg`}
+                                alt="sign"
+                            />
+                        )}
+                        {file.emo && (
+                            <img
+                                src={`${imageSrc}assets/PrivateCabinet/smiles/${file.emo}.svg`}
+                                alt="emoji"
+                            />
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>}
+    </>
+    )
+}
+
+export default FileInfo
