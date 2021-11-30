58c3406d61cb7dcd8436078d37cc9c415a6f1fba
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2021-11-30 06:27:32 +0200

    added for several files FileInfo => generalComponents

--- src/generalComponents/FileInfo/FileInfo.js
+++ src/generalComponents/FileInfo/FileInfo.js
@@ -9,11 +9,11 @@ function FileInfo({file = {}}) {
         <>
             {file === {} ? null : <div className={styles.wrap}>
             <div className={styles.innerFileWrap}>
-                <File color={file.id_color} format={file.ext} />
-                {file.is_pass ? <img className={styles.lock} src={`${imageSrc}assets/PrivateCabinet/locked.svg`} alt='lock' /> : null}
+                <File color={file?.id_color} format={file?.ext ?? 'FILE'} />
+                {file?.is_pass ? <img className={styles.lock} src={`${imageSrc}assets/PrivateCabinet/locked.svg`} alt='lock' /> : null}
             </div>
             <div className={styles.descriptionWrap}>
-                <div className={styles.fileName}>{file.name}</div>
+                <div className={styles.fileName}>{file?.name}</div>
                 <div className={styles.innerFileInfo}>
                     <div className={styles.fileSize}>{file?.size_now}</div>
                     <div className={styles.descriptionGroup}>
@@ -23,23 +23,24 @@ function FileInfo({file = {}}) {
                                 [styles.ftag]: !!file?.tag,
                             })}
                         >
-                            {file?.tag && `#${file.tag}`}
+                            {file?.tag && `#${file?.tag}`}
                         </div>
                         {file.fig && (
                             <img
-                                src={`${imageSrc}assets/PrivateCabinet/signs/${file.fig}.svg`}
+                                src={`${imageSrc}assets/PrivateCabinet/signs/${file?.fig}.svg`}
                                 alt="sign"
                             />
                         )}
                         {file.emo && (
                             <img
-                                src={`${imageSrc}assets/PrivateCabinet/smiles/${file.emo}.svg`}
+                                src={`${imageSrc}assets/PrivateCabinet/smiles/${file?.emo}.svg`}
                                 alt="emoji"
                             />
                         )}
                     </div>
                 </div>
             </div>
+                {file?.ext === 'FILES' ? <div className={styles.severalChosen}>Выбранно {file?.count} файлов/папок</div> : null}
         </div>}
     </>
     )
