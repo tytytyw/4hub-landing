e4dac378b8646f525f6d3d9780d24e55914d7194
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-01-07 11:08:50 +0200

    clear comment PreviewFile

--- src/Pages/Cabinet/Components/PreviewFile/PreviewFile.js
+++ src/Pages/Cabinet/Components/PreviewFile/PreviewFile.js
@@ -31,11 +31,7 @@ const PreviewFile = ({setFilePreview, file, share}) => {
 
     const canvasRef = useRef(null)
     const canvasWrapRef = useRef(null)
-    // const dotRightRef = useRef(null)
-    // const dotLeftRef = useRef(null)
-    // const lineRef = useRef(null)
     const audioRef = useRef(null);
-    // const [textDraw, setTextDraw] = useState({edit: false, text: 'Текст', move: false, widthDif: 0, heightDif: 0, sizeChange: false, initialParams: {x: 0, y: 0, b: 0, c: 0}, axis: null})
     const [audio, setAudio] = useState(null);
     const [video, setVideo] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
@@ -47,13 +43,11 @@ const PreviewFile = ({setFilePreview, file, share}) => {
                         canvasRef={canvasRef}
                         canvasWrapRef={canvasWrapRef}
                         file={file}
-                        // setTextDraw={setTextDraw}
                         setFilePreview={setFilePreview}
                         share={share}
                         direction="row"
                         drawParams={drawParams}
                         setDrawParams={setDrawParams}
-                        // unDoPaint={() => unDoPaintBrush(canvasRef, undoList, setUndoList)}
                     />
                     <div className={styles.canvasWrap}>
                         {isLoading ? <Loader
@@ -68,22 +62,7 @@ const PreviewFile = ({setFilePreview, file, share}) => {
                         <canvas
                             ref={canvasRef}
                             className={styles.canvas}
-                            // onMouseDown={mouseDownHandler}
                         />
-                        {/*{drawParams.figure === "arrow-outlined" ? <div*/}
-                        {/*    ref={lineRef}*/}
-                        {/*    className={styles.arrowOutlined}*/}
-                        {/*    onMouseDown={handleMouseDown}*/}
-                        {/*    draggable={false}*/}
-                        {/*    style={{*/}
-                        {/*        height: drawParams.width,*/}
-                        {/*        background: `linear-gradient(90deg, ${drawParams.color} 0%, ${drawParams.color} 99%, rgba(0, 0, 0, 0) 99%)`*/}
-                        {/*    }}*/}
-                        {/*>*/}
-                        {/*    <span className={styles.arrow} style={{border: `${drawParams.width + 9}px solid transparent`, borderLeft: `${drawParams.width + 9}px solid ${drawParams.color}`, right: -(drawParams.width + 9)}} />*/}
-                        {/*    <span className={styles.dotRight} style={{height: 6, width: 6, right: -6}} ref={dotRightRef} draggable={false} />*/}
-                        {/*    <span className={styles.dotLeft} style={{height: 6, width: 6, left: -6}} ref={dotLeftRef} draggable={false} />*/}
-                        {/*</div> : null}*/}
                     </div>
                 </div>
             }
@@ -143,63 +122,12 @@ const PreviewFile = ({setFilePreview, file, share}) => {
     }, []); //eslint-disable-line
 
     const [drawParams, setDrawParams] = useState({color: 'black', colorRGBA: 'rgba(0, 0, 0, 0.2)', width: 2, imgWidth: 0, imgHeight: 0, figure: "brush-outlined", fontSize: 13, fontFamily: 'Arial, sans-serif', lineHeight: 15});
-    // const [undoList, setUndoList] = useState([]);
-
-    // const mouseDownHandler = e => {
-    //     if(drawParams.figure === "arrow-outlined") drawDiv(canvasRef, lineRef, setUndoList, setTextDraw, setDrawParams);
-    // }
-
-    // const handleMouseDown = e => {
-        // if(drawParams.figure === "arrow-outlined") {
-        //     let axis = null;
-        //     if(e.target.className === styles.dotLeft) {
-        //         const params = dotRightRef.current.getBoundingClientRect();
-        //         axis = {y: params.top + params.height/2, x: params.left + params.width/2};
-        //     }
-        //     if(e.target.className === styles.dotRight) {
-        //         const params = dotLeftRef.current.getBoundingClientRect();
-        //         axis = {y: params.top + params.height/2, x: params.left + params.width/2};
-        //     }
-        //     let isCircle = false;
-        //     e.nativeEvent.path.forEach(el => {if(el.className && el.className.includes('dot')) isCircle = true})
-        //     if(isCircle) {
-        //         setTextDraw(state => ({...state, move: false, widthDif: e.nativeEvent.layerX, heightDif: e.nativeEvent.layerY, sizeChange: true, initialParams: {x: e.pageX, y: state.initialParams.y === 0 ? e.pageY : state.initialParams.y}, axis}))
-        //     } else {
-        //         setTextDraw(state => ({...state, move: true, widthDif: e.nativeEvent.layerX, heightDif: e.nativeEvent.layerY}))
-        //     }
-        // }
-    // }
-
-    // const handlePosition = e => {
-        // if(drawParams.figure === "arrow-outlined") {
-        //     if(textDraw.move) {
-        //         lineRef.current.style.left = e.pageX - canvasRef.current.getBoundingClientRect().x - textDraw.widthDif + "px";
-        //         lineRef.current.style.top = e.pageY - canvasRef.current.getBoundingClientRect().y - textDraw.heightDif + "px";
-        //     }
-        //     if(textDraw.sizeChange) {
-        //         const arrow = lineRef.current.getBoundingClientRect()
-        //         const arrowEndX = arrow.left + arrow.width;
-        //         const b = textDraw.initialParams.y - e.pageY;
-        //         const c = e.pageX <= textDraw.axis.x ? -((arrow.left + arrow.width) - e.pageX) : e.pageX - arrow.left;
-        //         const a = Math.round(Math.sqrt(b*b + c*c));
-        //         if(a !== 0 && b !== 0 && c !== 0) {
-        //             const degree = Math.round(Math.atan(c / b) * 180 / Math.PI);
-        //             lineRef.current.style.transformOrigin = `center left`;
-        //             lineRef.current.style.transform = `rotate(${b > 0 ? degree - 90 : degree + 90}deg)`;
-        //             lineRef.current.style.width = (a - 3) + "px";
-        //         } else {
-        //             lineRef.current.style.width = (arrow.width + e.pageX - arrowEndX - 3) + "px";
-        //         }
-        //     }
-        // }
-    // }
 
     return (
         <PopUp set={set} background={'none'} padding='0'>
             <div
                 className={styles.preview}
                 onClick={set}
-                // onMouseUp={() => setTextDraw(state => ({...state, move: false, widthDif: 0, heightDif: 0}))}
             >
                 {loading
                     ? <Loader
