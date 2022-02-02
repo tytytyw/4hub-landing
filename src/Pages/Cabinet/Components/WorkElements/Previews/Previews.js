d8b632c9c81fe4e2287385e9bcc28a3cbdc44ed9
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-02-02 13:51:58 +0200

    added Previews to WorkElements

---
+++ src/Pages/Cabinet/Components/WorkElements/Previews/Previews.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Previews.module.sass';
+
+function Previews() {
+    return(
+        <div className={styles.previewsWrap}>
+
+        </div>
+    )
+}
+
+export default Previews
