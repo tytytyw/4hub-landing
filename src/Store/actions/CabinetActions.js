c39a64b601f11fd67db5b02bc668cd0a1bb1d9ea
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-03-31 22:54:36 +0300

    fixed warnings

--- src/Store/actions/CabinetActions.js
+++ src/Store/actions/CabinetActions.js
@@ -631,21 +631,20 @@ export const onChooseProjectFiles = (folder, project, page) => async (
   api
     .get(url)
     .then(res => {
-      console.log(res);
-      // if (res.data.ok === 1) {
-      //   dispatch({
-      //     type: LOAD_PROJECT_FILES,
-      //     payload: res.data.files
-      //   });
-      // } else {
-      //   return {
-      //     type: SET_MODALS,
-      //     payload: {
-      //       key: "erro",
-      //       value: { open: true, message: "Failed to load project files" }
-      //     }
-      //   };
-      // }
+      if (res.data.ok === 1) {
+        dispatch({
+          type: LOAD_PROJECT_FILES,
+          payload: res.data.files
+        });
+      } else {
+        return {
+          type: SET_MODALS,
+          payload: {
+            key: "erro",
+            value: { open: true, message: "Failed to load project files" }
+          }
+        };
+      }
     })
     .catch(() => ({
       type: SET_MODALS,
