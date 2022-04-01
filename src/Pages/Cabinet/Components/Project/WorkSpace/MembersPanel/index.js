365a3bad3e35da5699c0f9d4c3a2ea6eda08d034
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-04-01 19:34:28 +0300

    fixed eslint errors

--- src/Pages/Cabinet/Components/Project/WorkSpace/MembersPanel/index.js
+++ src/Pages/Cabinet/Components/Project/WorkSpace/MembersPanel/index.js
@@ -1,47 +1,51 @@
-import React from 'react'
+import React from "react";
 
-import styles from './MembersPanel.module.sass'
+import styles from "./MembersPanel.module.sass";
 import classNames from "classnames";
-import {imageSrc} from '../../../../../../generalComponents/globalVariables';
+import { imageSrc } from "../../../../../../generalComponents/globalVariables";
+import PropTypes from "prop-types";
 
-const MembersPanel = ({setAddMember}) => {
+const MembersPanel = ({ setAddMember }) => {
+  const members = [
+    { user: "", img: "a1", active: false },
+    { user: "", img: "a2", active: true },
+    { user: "", img: "a3", active: true },
+    { user: "", img: "a4", active: false },
+    { user: "", img: "a2", active: true },
+    { user: "", img: "a1", active: false },
+    { user: "", img: "a3", active: true }
+  ];
 
-    const members = [
-        {user: '', img: 'a1', active: false},
-        {user: '', img: 'a2', active: true},
-        {user: '', img: 'a3', active: true},
-        {user: '', img: 'a4', active: false},
-        {user: '', img: 'a2', active: true},
-        {user: '', img: 'a1', active: false},
-        {user: '', img: 'a3', active: true},
-    ]
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles.list}>
+        <li
+          onClick={() => setAddMember(true)}
+          className={classNames(styles.addMemberBtn, styles.item)}
+        >
+          <img
+            className={styles.icon}
+            src={`${imageSrc}assets/PrivateCabinet/add-member.svg`}
+            alt="Add Member"
+          />
+        </li>
+        {members?.map((member, index) => (
+          <li className={styles.item} key={index}>
+            <img
+              className={styles.itemImg}
+              src={`${imageSrc}assets/PrivateCabinet/avatars/${member?.img}.svg`}
+              alt={member?.user}
+            />
+            {member?.active && <span className={styles.active} />}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-    return (
-        <div className={styles.wrapper}>
-            <ul className={styles.list}>
-                <li
-                    onClick={() => setAddMember(true)}
-                    className={classNames(styles.addMemberBtn, styles.item)}
-                >
-                    <img
-                        className={styles.icon}
-                        src={`${imageSrc}assets/PrivateCabinet/add-member.svg`}
-                        alt="Add Member"
-                    />
-                </li>
-                {members?.map((member, index) => (
-                    <li className={styles.item} key={index}>
-                        <img
-                            className={styles.itemImg}
-                            src={`${imageSrc}assets/PrivateCabinet/avatars/${member?.img}.svg`}
-                            alt={member?.user}
-                        />
-                        {member?.active && <span className={styles.active}/>}
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
-}
+export default MembersPanel;
 
-export default MembersPanel
+MembersPanel.propTypes = {
+  setAddMember: PropTypes.func
+};
