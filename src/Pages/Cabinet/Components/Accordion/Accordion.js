4b1492e8bf36dc9f0f162c9d334537b628aff29f
Vitaliy <vit.zkh@gmail.com>
2022-04-21 11:43:34 +0300

    Accordion.js ESlint

--- src/Pages/Cabinet/Components/Accordion/Accordion.js
+++ src/Pages/Cabinet/Components/Accordion/Accordion.js
@@ -1,68 +1,64 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
-import styles from './Accordion.module.sass'
-import classnames from 'classnames'
-import {useLocales} from "react-localized";
-
-
+import styles from "./Accordion.module.sass";
+import classnames from "classnames";
+import { useLocales } from "react-localized";
 
 const Accordion = () => {
-
-    const { __ } = useLocales()
-    const data = [
-        {id: 'question_1', question: __('Что такое 4Hub ?'), answer: 'Ответ 1'},
-        {id: 'question_2', question: __('Как увеличить обьем хранилища?'), answer: 'Ответ 2'},
-        {id: 'question_3', question: __('Как защитить мои данные?'), answer: 'Ответ 3'},
-    ]
-
-    const [active, setActive] = useState('')
-
-    const onAccordionClick = (item) => {
-        if (active === item.id)
-            setActive('')
-        else
-            setActive(item.id)
+  const { __ } = useLocales();
+  const data = [
+    { id: "question_1", question: __("Что такое 4Hub ?"), answer: "Ответ 1" },
+    {
+      id: "question_2",
+      question: __("Как увеличить обьем хранилища?"),
+      answer: "Ответ 2"
+    },
+    {
+      id: "question_3",
+      question: __("Как защитить мои данные?"),
+      answer: "Ответ 3"
     }
+  ];
 
-    const isActive = item => {
-        return active === item.id
-    }
+  const [active, setActive] = useState("");
 
-    return (
-        <div className={styles.wrapper}>
+  const onAccordionClick = item => {
+    if (active === item.id) setActive("");
+    else setActive(item.id);
+  };
 
-            <ul className={styles.accordion}>
+  const isActive = item => {
+    return active === item.id;
+  };
 
-                {data.map((item, index) => (
-                    <li className={styles.item} key={index}>
+  return (
+    <div className={styles.wrapper}>
+      <ul className={styles.accordion}>
+        {data.map((item, index) => (
+          <li className={styles.item} key={index}>
+            <div className={styles.header}>
+              <h4 className={styles.question}>{item.question}</h4>
+              <span
+                onClick={() => onAccordionClick(item)}
+                className={classnames({
+                  [styles.openBtn]: true,
+                  [styles.plusBtn]: !isActive(item)
+                })}
+              />
+            </div>
 
-                        <div className={styles.header}>
-                            <h4 className={styles.question}>{item.question}</h4>
-                            <span
-                                onClick={() => onAccordionClick(item)}
-                                className={classnames({
-                                    [styles.openBtn]: true,
-                                    [styles.plusBtn]: !isActive(item),
-                                })}
-                            />
-                        </div>
+            <div
+              className={classnames({
+                [styles.content]: true,
+                [styles.toggled]: isActive(item)
+              })}>
+              <div className={styles.answer}>{item.answer}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-                        <div className={classnames({
-                            [styles.content]: true,
-                            [styles.toggled]: isActive(item)
-                        })}>
-                            <div className={styles.answer}>
-                                {item.answer}
-                            </div>
-                        </div>
-
-                    </li>
-                ))}
-
-            </ul>
-
-        </div>
-    )
-}
-
-export default Accordion
+export default Accordion;
