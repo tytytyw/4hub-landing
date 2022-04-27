1c7cd32760b88d1d65abac546873a15563db18cd
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-04-27 06:58:47 +0300

    created FileAccessRights Modal

---
+++ src/Pages/Cabinet/Components/Modals/Components/FileAccessRights/FileAccessRights.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./FileAccessRights.module.sass";
+
+function FileAccessRights() {
+  return <div className={styles.fileAccessRightsWrap}></div>;
+}
+
+export default FileAccessRights;
