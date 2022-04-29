29e328462797afa4a913ef9ce0a4e129eb53f673
Vitaliy <vit.zkh@gmail.com>
2022-04-29 16:43:03 +0300

    Company Types

---
+++ src/types/Company.js
@@ -0,0 +1,32 @@
+import PropTypes from "prop-types";
+
+export const itemProps = PropTypes.exact({
+  children: PropTypes.array,
+  name: PropTypes.string,
+  icon: PropTypes.object,
+  label: PropTypes.string
+});
+
+const personInfoProps = PropTypes.exact({
+  name: PropTypes.string,
+  surname: PropTypes.string,
+  middleName: PropTypes.string,
+  position: PropTypes.string,
+  status: PropTypes.object,
+  phone: PropTypes.string,
+  phone2: PropTypes.string,
+  email: PropTypes.string,
+  email2: PropTypes.string
+});
+
+export const personProps = PropTypes.exact({
+  data: PropTypes.shape({
+    info: personInfoProps
+  }),
+  id: PropTypes.string,
+  position: PropTypes.shape({
+    x: PropTypes.number,
+    y: PropTypes.number
+  }),
+  type: PropTypes.string
+});
