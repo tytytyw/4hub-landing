de433d8937a8ab5dac38c6de1afa47ee9a66a3ef
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-03 07:21:58 +0300

    created FileAccessEdit component

---
+++ src/Pages/Cabinet/Components/Modals/Components/FileAccessRights/FileAccessUserList/FileAccessEdit/FileAccessEdit.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./FileAccessEdit.module.sass";
+
+function FileAccessEdit({}) {
+  return <div className={styles.fileAccessWrap}>FileAccess works!</div>;
+}
+
+export default FileAccessEdit;
