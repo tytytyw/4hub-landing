ac004604268c2cc9e242ac3ac099ad8d5d4ff45c
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-12 08:55:53 +0300

    created Tasks component

---
+++ src/Pages/Cabinet/Tasks/Tasks.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Tasks.module.sass";
+
+function Tasks() {
+  return <div className={styles.taskWrap}>Tasks works!</div>;
+}
+
+export default Tasks;
