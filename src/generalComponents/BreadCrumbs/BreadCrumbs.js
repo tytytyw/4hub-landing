73b85ffbc7865140739ceb127e59661ee2251385
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-12 14:53:25 +0300

    added ManagementPanel && Breadcrumbs

---
+++ src/generalComponents/BreadCrumbs/BreadCrumbs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BreadCrumbs.module.sass";
+
+function BreadCrumbs() {
+  return <div className={styles.breadCrumbsWrap}></div>;
+}
+
+export default BreadCrumbs;
