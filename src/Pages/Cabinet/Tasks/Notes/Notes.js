765512b725bfe69dea5d797fe66096688ba0a2f5
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-12 10:41:26 +0300

    added empty note

--- src/Pages/Cabinet/Tasks/Notes/Notes.js
+++ src/Pages/Cabinet/Tasks/Notes/Notes.js
@@ -1,8 +1,26 @@
 import React from "react";
 import styles from "./Notes.module.sass";
+import { useLocales } from "react-localized";
+import { ReactComponent as AddIcon } from "../../../../assets/PrivateCabinet/plus-3.svg";
+import { imageSrc } from "../../../../generalComponents/globalVariables";
 
 function Notes() {
-  return <div className={styles.notesWrap}></div>;
+  const { __ } = useLocales();
+
+  return (
+    <div className={styles.notesWrap}>
+      <div className={styles.emptyNote}>
+        <AddIcon className={styles.addIcon} />
+        <span className={styles.text}>{__("Создать заметку")}</span>
+        <img
+          src={`${imageSrc}assets/PrivateCabinet/tasks/corners/corner-grey.svg`}
+          alt="img"
+          className={styles.corner}
+        />
+      </div>
+      <></>
+    </div>
+  );
 }
 
 export default Notes;
