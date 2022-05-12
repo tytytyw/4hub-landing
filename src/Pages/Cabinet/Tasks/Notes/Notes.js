cebe06abfc788f695658f29ed125124222dea0d5
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-12 09:21:20 +0300

    added Notes to Tasks

---
+++ src/Pages/Cabinet/Tasks/Notes/Notes.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Notes.module.sass";
+
+function Notes() {
+  return <div className={styles.notesWrap}></div>;
+}
+
+export default Notes;
--- src/Pages/Cabinet/Tasks/Tasks.js
+++ src/Pages/Cabinet/Tasks/Tasks.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Tasks.module.sass";
 import Header from "./Header/Header";
+import Notes from "./Notes/Notes";
 
 function Tasks() {
   return (
     <div className={styles.taskWrap}>
       <Header />
+      <Notes />
     </div>
   );
 }
