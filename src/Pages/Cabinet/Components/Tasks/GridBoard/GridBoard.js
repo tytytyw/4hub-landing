6869031c9f55c7a47840df1d2532fd3e7861b48c
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-13 17:01:03 +0300

    created GridBoard

---
+++ src/Pages/Cabinet/Components/Tasks/GridBoard/GridBoard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./GridBoard.module.sass";
+
+function GridBoard() {
+  return <div className={styles.girdBoardWrap}>Works!</div>;
+}
+
+export default GridBoard;
