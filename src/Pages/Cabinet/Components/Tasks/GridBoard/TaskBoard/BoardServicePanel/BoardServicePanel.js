2a16da09698aca9a7ac6a47ef4ebc08165e8e7b8
Max Kortelov <kortelevmax@fromzerotowebhero.ru>
2022-05-14 01:59:49 +0300

    added TabPicker

--- src/Pages/Cabinet/Components/Tasks/GridBoard/TaskBoard/BoardServicePanel/BoardServicePanel.js
+++ src/Pages/Cabinet/Components/Tasks/GridBoard/TaskBoard/BoardServicePanel/BoardServicePanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./BoardServicePanel.module.sass";
 import PropTypes from "prop-types";
 import { BOARDS } from "../../../../../../../generalComponents/globalVariables";
@@ -7,17 +7,28 @@ import { ReactComponent as FrameIcon } from "assets/PrivateCabinet/tasks/frame.s
 import classNames from "classnames";
 import { ReactComponent as AddIcon } from "../../../../../../../assets/PrivateCabinet/plus-3.svg";
 import ThreeDots from "../../../../../../../generalComponents/ThreeDots/ThreeDots";
+import { ReactComponent as LinesIcon } from "assets/PrivateCabinet/lines.svg";
+import { ReactComponent as BarsIcon } from "assets/PrivateCabinet/tasks/bars.svg";
+import { ReactComponent as CalendarIcon } from "assets/PrivateCabinet/tasks/calendar.svg";
+import { ReactComponent as ListIcon } from "assets/PrivateCabinet/tasks/list.svg";
+import TabsPicker from "../../../../../../../generalComponents/TabsPicker/TabsPicker";
 
-function BoardServicePanel({ type, isLast }) {
+function BoardServicePanel({ type, isLastElement }) {
   const TITLE = useTaskBoardTitle();
+  const [tabSelect, setTabSelect] = useState(2);
+
+  const ELEMENTS = [ListIcon, BarsIcon, LinesIcon, CalendarIcon];
 
   return (
     <div className={styles.boardServicePanelWrap}>
       <span className={styles.boardTitle}>{TITLE[type]}</span>
       <div className={styles.buttonsWrap}>
-        <FrameIcon />
-        {!isLast ? <AddIcon className={classNames(styles.addIcon)} /> : null}
-        <ThreeDots />
+        <FrameIcon className={styles.frameIcon} />
+        {!isLastElement ? <AddIcon className={classNames(styles.addIcon)} /> : null}
+        {isLastElement ? <TabsPicker ELEMENTS={ELEMENTS} selected={tabSelect} onClick={setTabSelect} /> : null}
+        <div className={styles.threeDots}>
+          <ThreeDots onClick={() => {}} />
+        </div>
       </div>
     </div>
   );
@@ -27,5 +38,5 @@ export default BoardServicePanel;
 
 BoardServicePanel.propTypes = {
   type: PropTypes.oneOf(Object.values(BOARDS)).isRequired,
-  isLast: PropTypes.bool
+  isLastElement: PropTypes.bool
 };
---
+++ src/generalComponents/TabsPicker/TabsPicker.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./TabsPicker.module.sass";
+import classNames from "classnames";
+import { useSelector } from "react-redux";
+import PropTypes, { element } from "prop-types";
+
+function TabsPicker({ ELEMENTS, selected, onClick }) {
+  const { theme } = useSelector((s) => s.user.userInfo);
+
+  return (
+    <div className={styles.viewPanel}>
+      {ELEMENTS.map((Element, i) => (
+        <div
+          key={i}
+          onClick={() => onClick(i)}
+          className={classNames(`interaction-background-${theme}`, {
+            [styles.iconViewChosen]: selected === i,
+            [`background-${theme}`]: selected === i,
+            [styles.iconView]: selected !== i
+          })}
+        >
+          <Element />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TabsPicker;
+
+TabsPicker.defaultProps = {
+  ELEMENTS: [],
+  selected: -1,
+  onClick: () => {}
+};
+
+TabsPicker.propTypes = {
+  ELEMENTS: PropTypes.arrayOf(element).isRequired,
+  selected: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired
+};
