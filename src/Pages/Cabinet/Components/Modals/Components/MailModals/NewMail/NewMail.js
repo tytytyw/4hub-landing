c48afcae94191ad5e8ffa477314a4a46bca1a169
Vitaliy <vit.zkh@gmail.com>
2022-05-27 11:09:10 +0300

    dell comments

--- src/Pages/Cabinet/Components/Modals/Components/MailModals/NewMail/NewMail.js
+++ src/Pages/Cabinet/Components/Modals/Components/MailModals/NewMail/NewMail.js
@@ -1,15 +1,10 @@
 import React from "react";
 import styles from "./NewMail.module.sass";
-import { MAIL_MODALS } from "../../../../../../../generalComponents/globalVariables";
-import PropTypes from "prop-types";
-import { addNoteParams } from "../../../../../../../types/Tasks";
 import MailBottomMenu from "Pages/Cabinet/Components/Mail/Components/MianBottomMenu/MailBottomMenu";
-// import { useDispatch } from "react-redux";
 import { useLocales } from "react-localized";
-// type, params, closeModal
+
 function NewMail() {
   const { __ } = useLocales();
-  // const dispatch = useDispatch();
   return (
     <div className={styles.newMailWrap}>
       <div className={styles.customInput}>
@@ -34,12 +29,6 @@ function NewMail() {
 
 export default NewMail;
 
-NewMail.defaultProps = {
-  closeModal: () => {}
-};
+NewMail.defaultProps = {};
 
-NewMail.propTypes = {
-  type: PropTypes.oneOf(Object.values(MAIL_MODALS)).isRequired,
-  params: addNoteParams.isRequired,
-  closeModal: PropTypes.func
-};
+NewMail.propTypes = {};
